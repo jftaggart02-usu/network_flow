9c645c5e7243687cea8c49805e4429de4e2671dd
Joshua Taggart <jftaggart02@gmail.com>
2026-04-14 20:34:10 -0600

    initial commit

---
+++ src/Graph.java
@@ -0,0 +1,61 @@
+public class Graph {
+    private final GraphNode[] vertices;  // Adjacency list for graph.
+    private final String name;  //The file from which the graph was created.
+
+    public Graph(String name, int vertexCount) {
+        this.name = name;
+
+        vertices = new GraphNode[vertexCount];
+        for (int vertex = 0; vertex < vertexCount; vertex++) {
+            vertices[vertex] = new GraphNode(vertex);
+        }
+    }
+
+    public boolean addEdge(int source, int destination, int capacity) {
+        // A little bit of validation
+        if (source < 0 || source >= vertices.length) return false;
+        if (destination < 0 || destination >= vertices.length) return false;
+
+        // This adds the actual requested edge, along with its capacity
+        vertices[source].addEdge(source, destination, capacity);
+
+        // TODO: This is what you have to describe in the required README.TXT file
+        //       that you submit as part of this assignment.
+        vertices[destination].addEdge(destination, source, 0);
+
+        return true;
+    }
+
+    /**
+     * Algorithm to find max-flow in a network
+     */
+    public int findMaxFlow(int s, int t, boolean report) {
+        // TODO:
+        return 0;
+    }
+
+    /**
+     * Algorithm to find an augmenting path in a network
+     */
+    private boolean hasAugmentingPath(int s, int t) {
+        // TODO:
+        return false;
+    }
+
+    /**
+     * Algorithm to find the min-cut edges in a network
+     */
+    public void findMinCut(int s) {
+        // TODO:
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("The Graph " + name + " \n");
+        for (var vertex : vertices) {
+            sb.append((vertex.toString()));
+        }
+        return sb.toString();
+    }
+}
